beae8188d7a4fe6a8a28f1bbd45fc46506f0e796
Felipe Rocha <felipemota.rocha@gmail.com>
2023-04-08 22:16:12 -0300

    profiling: initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,47 +1,27 @@
 import { useState } from "react";
+import Counter from "./components/Counter";
 import Theme from "./components/Theme";
-import { products } from "./data/products";
 
 function App() {
+  const [count, setCount] = useState(0);
   const [theme, setTheme] = useState("light");
-  const [searchQuery, setSearchQuery] = useState<string>("");
 
   const toggleTheme = () => {
     setTheme((prev) => (prev === "light" ? "dark" : "light"));
   };
 
-  const getFilteredProducts = () => {
-    console.log("Filtering products...");
-    return products.filter((product: any) => {
-      return product.name.toLowerCase().includes(searchQuery.toLowerCase());
-    });
-  };
+  const increaseCount = () => setCount((prev) => prev + 1);
 
   return (
     <div>
-      <Theme theme={theme} />
+      <Counter count={count} />
+
+      <Theme theme={theme} toggleTheme={toggleTheme} />
 
       <div className="flex gap-6">
-        <button onClick={toggleTheme} className="mt-6 bg-red-600 text-white rounded p-4">
-          Toggle Theme
+        <button onClick={increaseCount} className="mt-6 bg-blue-600 text-white rounded p-4">
+          Increase Count
         </button>
-      </div>
-
-      <input
-        className="p-2 rounded mt-4 border-2 border-solid border-blue-600"
-        type="text"
-        placeholder="Filter products..."
-        value={searchQuery}
-        onChange={(e: React.ChangeEvent<HTMLInputElement>) => setSearchQuery(e.target.value)}
-      />
-
-      <div className="mt-6 border-solid p-5 border-4 border-blue-600 rounded">
-        <h1 className="text-3xl font-bold">Products</h1>
-        <p className="text-xl">Re-render {(Math.random() * 100).toFixed(0)}</p>
-
-        {getFilteredProducts().map((product) => (
-          <p>{product.name}</p>
-        ))}
       </div>
     </div>
   );
--- src/components/Theme.tsx
+++ src/components/Theme.tsx
@@ -1,10 +1,14 @@
 import { memo } from "react";
 
-function Theme(props: { theme: string }) {
+function Theme(props: { theme: string; toggleTheme: () => void }) {
   return (
     <div className="mt-6 border-solid p-5 border-4 border-red-600 rounded">
       <h1 className="text-3xl font-bold">Theme: {props.theme}</h1>
       <p className="text-xl">Re-render {(Math.random() * 100).toFixed(0)}</p>
+
+      <button onClick={props.toggleTheme} className="mt-6 bg-red-600 text-white rounded p-4">
+        Toggle Theme
+      </button>
     </div>
   );
 }
